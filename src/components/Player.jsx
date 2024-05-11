a2fde93f9b83dabae1cf340b4039d09fb09b2b16
Anish Bade <badeanish6045@gmail.com>
2024-05-11 14:57:35 +0545

    upating state based on old state correctly

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,12 +2,16 @@ import { useState } from "react";
 
 export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
+
   function handleEditClick() {
-    setIsEditing(true);
+    setIsEditing((editing) => !editing);
   }
   let playerName = <span className="player-name"> {name}</span>;
-  if (isEditing){
-    playerName=<input type="text" placeholder={name} required></input>;
+  let btnCaption = "Edit";
+
+  if (isEditing) {
+    playerName = <input type="text" value={name} required></input>;
+    btnCaption = "Save";
   }
   return (
     <li>
@@ -15,7 +19,7 @@ export default function Player({ name, symbol }) {
         {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={() => handleEditClick()}>Edit</button>
+      <button onClick={() => handleEditClick()}>{btnCaption}</button>
     </li>
   );
 }
